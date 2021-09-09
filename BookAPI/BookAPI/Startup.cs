679256e58a4d4422f0201430de8d5215b0d9a667
NAME <EMAIL>
2021-09-09 17:06:17 +0800

    im done

--- BookAPI/BookAPI/Startup.cs
+++ BookAPI/BookAPI/Startup.cs
@@ -2,15 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+
 
 namespace BookAPI
 {
@@ -28,6 +30,8 @@ namespace BookAPI
         {
 
             services.AddControllers();
+            services.AddDbContext<BookContext>(opt =>
+                                               opt.UseInMemoryDatabase("Books"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookAPI", Version = "v1" });
